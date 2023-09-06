9f0cdb0f9cc0dd78bcc9056fcef1f93acca2c30d
YanaPrilipko <plilipkoyana@gmail.com>
2023-09-06 18:08:48 +0300

    Lesson 4

---
+++ Program/Program/Operators.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class Operators
+    {
+        public static void Parcer()
+        {
+            double sum1 = 0;
+            double sum2 = 0;
+            double x;
+            double y;
+            Console.Write("Enter the digit of the initial value of Х?: ");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.Write("Enter the digit of the final value of x?: ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            if (number1 < number2)
+            {
+                 x = number1;
+                 y = number2;
+            }
+            else
+            {
+                 x = number2;
+                 y = number1;
+            }
+
+
+            for (double i = x; i <= y; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum1 += i;
+                }
+
+                else
+                {
+                    sum2 += i;
+                } 
+            }
+            Console.WriteLine($" {sum1} even");
+            Console.WriteLine($" {sum2} not even ");
+        }
+    }
+}
--- Program/Program/Tupu.cs
+++ Program/Program/Tupu.cs
@@ -1,7 +1,9 @@
 ﻿
-double a = 4.3;
+using Student;
+
+/*double a = 4.3;
 short b = 10;
-decimal k = (decimal) (a + b);
+decimal k = (decimal)(a + b);
 Console.WriteLine(k);
 int c = 5;
 int l = c * b;
@@ -17,7 +19,7 @@ Console.WriteLine($"Sum of {number1} and {number2} is {number1 + number2}");
 
 Console.Write("Enter a number to calculate the circle: ");
 int x = int.Parse(Console.ReadLine());
-double pi = 2 *Math.PI * x;
+double pi = 2 * Math.PI * x;
 
 Console.WriteLine($"The length of the circle is {pi}");
 //DateTime dateTime = new DateTime.Now;
@@ -28,4 +30,7 @@ var difference = dateTime1.Subtract(DateTime.Now).Days;
 var difference1 = DateTime.Now.Subtract(dateTime2).Days;
 
 Console.WriteLine($"{difference} days left to New Year");
-Console.WriteLine($"{difference1} days passed from New Year");
+Console.WriteLine($"{difference1} days passed from New Year");*/
+
+
+Operators.Parcer();
